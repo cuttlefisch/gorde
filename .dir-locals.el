a6eebbb2cfb0c27d0e1d6d3664f246362e286d6e
Hayden Stanko <33015950+cuttlefisch@users.noreply.github.com>
2022-12-29 12:49:55 -0800

    Initial basic template (#1)
    * working out dir-locals, templating changes
    
    * add gitignore
    
    * remove org-roam.db
    
    * improve README

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,11 +1,20 @@
-((org-mode . ((eval . (setq-local project-root-path
+((nil . ((eval . (setq project-root-path
                         ;; Find out what dir we're in, and ensure we have
                         ;; it in string format.
                         (file-name-directory
                          (let ((curdir (dir-locals-find-file "./")))
                                 (if (stringp curdir)
                                     curdir
-                                   (car curdir))))))
+                                   (car curdir))))))))
+ (org-mode . (
+              ;; (eval . (setq-local project-root-path
+              ;;           ;; Find out what dir we're in, and ensure we have
+              ;;           ;; it in string format.
+              ;;           (file-name-directory
+              ;;            (let ((curdir (dir-locals-find-file "./")))
+              ;;                   (if (stringp curdir)
+              ;;                       curdir
+              ;;                      (car curdir))))))
 
           ;; These two are important, if not set correctly your REAL
           ;; org roam instance may be modified.
